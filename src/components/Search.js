3e3488dd4af1338b6f2d0c74b3b532625d26a0f1
Adam Finch <atyreefinch@gmail.com>
2019-12-04 14:26:19 -0500

    small image change before merge

--- src/components/Search.js
+++ src/components/Search.js
@@ -6,17 +6,17 @@ class Search extends React.Component {
 
     render() {
         return <div className={this.props.searchResults ? `landing-text-with-results row` : `landing-text row`}>
-            <div className="col"><h1 className="hero-text display-1 font-weight-bolder">{"<"}ReactToWork{"/>"}</h1>
-            <form onSubmit={this.props.submitSearch}>
-            <label htmlFor='search'></label>
-            <input type='text' placeholder="Enter a Zip for Jobs Near You" className='search-input' onChange={this.props.setSearch}></input>
-            <br></br>
-            <label htmlFor='RemoteOK'> Remote OK? </label>
-            <input className="checkbox" type='checkbox' name="RemoteOK" defaultChecked={this.props.includeRemote} onChange={this.props.setFromRemoteOK}></input>
-            <br></br>
-            <input type='submit' className='btn btn-primary' value='Search'></input>
-            </form>
-            </div>
+                <div className="col"><h1 className="hero-text display-1 font-weight-bolder">{"<"}ReactToWork{"/>"}</h1>
+                    <form onSubmit={this.props.submitSearch}>
+                        <label htmlFor='search'></label>
+                        <input type='text' placeholder="Enter a Zip for Jobs Near You" className='search-input' onChange={this.props.setSearch}></input>
+                        <br></br>
+                        <label htmlFor='RemoteOK'> Remote OK? </label>
+                        <input className="checkbox" type='checkbox' name="RemoteOK" defaultChecked={this.props.includeRemote} onChange={this.props.setFromRemoteOK}></input>
+                        <br></br>
+                        <input type='submit' className='btn btn-primary' value='Search'></input>
+                    </form>
+                </div>
         </div>
     }
 
