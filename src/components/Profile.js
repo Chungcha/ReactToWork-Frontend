65895098276c257994450bff42f194a75fe3b2f5
Adam Finch <atyreefinch@gmail.com>
2019-12-05 09:01:32 -0500

    home refactoring, profile page routing & form styling

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -6,11 +6,9 @@ class Profile extends React.Component {
 
 render() {
 return <div className={this.props.searchResults ? "home-background-with-results App container-fluid" : "home-background App container-fluid" } id="home">
-      <div className="container">
-          <div className ="row">
+      
 <div className ="col profile-info">{this.props.user && `${this.props.user.username}`}</div>
-          </div>
-      </div>
+     
       
       </div>
     }
