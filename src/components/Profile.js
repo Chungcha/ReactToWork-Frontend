ef6a544840372fd8229eda1866c62ce0d3af8758
chungcha <charlieschung@gmail.com>
2019-12-06 10:40:01 -0500

    added posted Jobs

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -9,19 +9,29 @@ class Profile extends React.Component {
 
 render() {
 return <div className="home-background-with-results App container-fluid" id="home">
-      
-<div className ="col profile-info"></div>
-<div className="card profile-page"><h1 className="hero-text display-1 font-weight-bolder"><FaUserAstronaut /></h1>
-    <div className="card profile-div-top">
-        <div className="card-body">
-            <h1 className="card-title">{this.props.user && this.props.user.username}</h1>
-<div className="row align-items-start profile-row"><strong>{"Email: "} </strong><span>&nbsp;</span>{this.props.user && `${this.props.user.email}`}</div>
-<div className="row profile-row profile-bio"><strong>{"Bio: "} </strong><span>&nbsp;</span>{this.props.user && this.props.user.bio}</div>
-<div className="row"><div className="col text-center "><h2>Your Saved Jobs:</h2></div>
-        </div></div>
+    <div className ="col profile-info">
     </div>
-     <ResultsContainer usersSavedJobs={this.props.usersSavedJobs} addToSavedJobs={this.props.addToSavedJobs} jobs={this.props.jobs}/>
-      </div>
+    <div className="card profile-page">
+        <h1 className="hero-text display-1 font-weight-bolder"><FaUserAstronaut /></h1>
+        <div className="card profile-div-top">
+            <div className="card-body">
+                <h1 className="card-title">{this.props.user && this.props.user.username}</h1>
+                <div className="row align-items-start profile-row"><strong>{"Email: "} </strong><span>&nbsp;</span>{this.props.user && `${this.props.user.email}`}</div>
+                <div className="row profile-row profile-bio"><strong>{"Bio: "} </strong><span>&nbsp;</span>{this.props.user && this.props.user.bio}</div>
+                <div className="row">
+                    <div className="col text-center "><h2>Your Saved Jobs:</h2></div>
+                </div>
+            </div>
+        </div>
+        <ResultsContainer usersSavedJobs={this.props.usersSavedJobs} addToSavedJobs={this.props.addToSavedJobs} jobs={this.props.jobs}/>
+        <div className="card profile-div-top">
+            <div className="card-body"></div>
+            <div className="row">
+                <div className="col text-center "><h2>Your Posted Jobs:</h2></div>
+            </div>
+                {this.props.user && this.props.user.posts ? <ResultsContainer usersSavedJobs={this.props.usersSavedJobs} addToSavedJobs={this.props.addToSavedJobs} jobs={this.props.user.posts}/> : <h1>no posts</h1>}
+            </div>
+    </div>
 </div>
     }
 }
